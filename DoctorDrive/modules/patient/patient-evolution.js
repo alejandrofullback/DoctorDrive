d610b209ccda904298989f1db4fc0d2771b4ab5e
carlos andres <kaitos90210@gmail.com>
2013-04-07 19:04:43 -0300

    adding patients first template

---
+++ DoctorDrive/modules/patient/patient-evolution.js
@@ -0,0 +1,10 @@
+﻿HomeViewModel = function () {
+    var self = this;
+    self.template = "patient/html/patient-evolution";
+
+    self.Actions = {};
+    self.Actions.startModule = function () {
+    };
+};
+
+HomeViewModel.id = "patient-evolution";
