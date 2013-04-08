e5046ddb75d299f3572701133c01720541f43dc1
Alejandro Osorio <alejandrofullback@gmail.com>
2013-04-07 21:41:58 -0300

    Add navigation

--- DoctorDrive/modules/menu/menu.js
+++ DoctorDrive/modules/menu/menu.js
@@ -5,9 +5,6 @@
     self.UI.Actions = {};
     self.UI.Actions.changePage = function (data, evt) {
         var id = $(evt.currentTarget).data('name');
-        if (id.indexOf("list") != -1) {
-            doctor_drive_globals.currentPage = 1;
-        }
         $(document).trigger('page-change', [id]);
     };
 
